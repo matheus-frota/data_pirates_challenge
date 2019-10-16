7b4f11aa3b7cb5833c81e6ebcf4d89fea34cab26
Matheus <matheusfrota@hotmail.com.br>
2019-10-15 23:05:19 -0300

    Adicionando modificações de verificação de diretorios, quantidade de fiilmes, tipo e json por genero

--- utilits.py
+++ utilits.py
@@ -3,13 +3,12 @@ import requests
 import pandas as pd
 import json
 from bs4 import BeautifulSoup
+import os
 
 def gerador_links():
     # Var. Auxiliar
-    #url_ = 'https://www.imdb.com/search/title/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=b9121fa8-b7bb-4a3e-8887-aab822e0b5a7&pf_rd_r=B8SXG6NH2MBERJ86XZG1&pf_rd_s=right-6&pf_rd_t=15506&pf_rd_i=moviemeter&ref_=chtmvm_gnr_1&genres=action&explore=title_type,genres'
     url_ = "https://www.imdb.com/chart/top?ref_=nv_mv_250"
     url = "https://www.imdb.com"
-
     links = []
 
     # Solicitação HTTP
@@ -40,13 +39,14 @@ def gerador_links():
 def info(numero_filmes = 500):
     # Chamando a função para gerar os links de cada gênero
     generos = gerador_links()
-    # Criando lista contendo a quantidade de links
-    qtd_film = [x+1 for x in range(0,numero_filmes,50)]
     # Url
     url = "https://www.imdb.com"
     
     for genero in generos:
-        print(genero)
+        # Imprime o gênero de cada grupo de filmes
+        nome_genero = genero.split("&")[0].split("=")[-1]
+        print("-------------------------------")
+        print("Gênero: {}".format(nome_genero))
         # Variável de controle da quantidade de iterações
         controle = 0
         # Dicionário utilizado para criar o JSONL por gênero
@@ -57,7 +57,7 @@ def info(numero_filmes = 500):
                 # pula para o próximo genero
                 if controle == numero_filmes:
                     # Cria um arquivo json com os filmes de cada gênero separadamente
-                    with open('teste.json', 'w') as json_file:
+                    with open('./dados/{}.json'.format(nome_genero), 'w') as json_file:
                         json.dump(json_genero, json_file)
                     break
                 # Solicitando acesso
@@ -73,27 +73,41 @@ def info(numero_filmes = 500):
                         # Dicionário criado a partir do nome de cada filme
                         json_genero ["{}".format(nome_filme)] = {}
                         # Armazenando o ano do filme
-                        json_genero["{}".format(nome_filme)]["ano"] = container.find("h3").text.split("\n")[3]
+                        json_genero["{}".format(nome_filme)]["ano"] = container.find("h3",class_="lister-item-header").find("span", class_="lister-item-year text-muted unbold").text.replace('(','').replace(')','')
                         # Armazenando a duração do filme
-                        json_genero["{}".format(nome_filme)]["duracao"] = container.select("p > .runtime")
+                        json_genero["{}".format(nome_filme)]["duracao"] = container.find("p", class_="text-muted").find("span",class_="runtime").text
                         # Armazenando o genero de cada filme
-                        json_genero["{}".format(nome_filme)]["tipo"] = container.select("p > .genre")
+                        json_genero["{}".format(nome_filme)]["tipo"] = container.find("p", class_="text-muted").find("span",class_="genre").text.rstrip()
                         # Armazenando a pontuação de cada filme IMDB
-                        json_genero["{}".format(nome_filme)]["pontuacao"] = container.select("div > strong")
+                        json_genero["{}".format(nome_filme)]["pontuacao"] = float(container.find("div",class_="ratings-bar").find("strong").text)
                         # COntador de filmes
                         controle+=1
+                        print("({},{})".format(controle,numero_filmes))
+
                     # Pegar links da próxima página - até o máximo ou até atingir 500
                     link_next = soup.find_all("div", class_="desc")[0]
-                    print(link_next)
                     # Condicionais para controlar as páginas que possuem link de next e
                     # as outras páginas que não tem link next.
                     if link_next.find("a", class_ = "lister-page-next next-page") != None:
                         genero = url + link_next.find("a", class_ = "lister-page-next next-page").get("href")
                     if link_next.find("a", class_ = "lister-page-next next-page") == None:
+                        with open('./dados/{}.json'.format(nome_genero), 'w') as json_file:
+                            json.dump(json_genero, json_file)
                         break
                 else:
                     print("{}".format(req.status_code))
             except ConnectionError:
                 break
+
+def main():
+    # Verificando se já existe diretorio para armazenar os arquivos json
+    if os.path.isdir("./dados/"): # vemos de este diretorio ja existe
+        print ('Ja existe uma pasta com esse nome!')
+    else:
+        os.mkdir("./dados/") # aqui criamos a pasta caso nao exista
+        print ('Pasta criada com sucesso!')
     
-info()
+    # Chamando função para montagem dos jsons
+    info()
+
+main()
